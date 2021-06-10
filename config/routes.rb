532429bc09e3dda0944c464dc05004c3df5a9eb6
jbrasko <jbrasko0508@gmail.com>
2021-06-10 11:00:49 -0700

    define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      # use slug instead of id as param for routing
+      resources :airlines, param: :slug
+      resources :reviews, only: [:create, :destroy]
+    end
+  end
+
+  # route requests for existing paths back to index path
+  get '*path', to: 'pages#index', via: :all
 end
